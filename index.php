6d4134baef33cdd504e9ea3f2160d540bf3a07f1
Alice Wanjiku <alicenjoroge707@thejitu.com>
2023-08-07 13:31:36 +0200

    functions and filters

--- index.php
+++ index.php
@@ -22,7 +22,8 @@ $books = [
     ]
 ];
 
-function filterByAuthor($books, $author){
+function filterByAuthor($books, $author)
+{
     $filteredBooks = [];
     foreach ($books as $book) {
         if ($book['author'] === $author) {
@@ -40,7 +41,44 @@ function filterByAuthor($books, $author){
             <?= $book['name']; ?>
         </li>
     <?php endforeach; ?>
+</ul>
 
+
+<?php
+//list of my favourite movies
+
+$movies = [
+    [
+        "name" => "cursed away",
+        "releaseYear" => 2019
+    ], [
+        "name" => "6 feet under",
+        "releaseYear" => 2011
+    ], [
+        "name" => "Mr and Mrs Smith",
+        "releaseYear" => 2012
+    ]
+];
+
+$filterByYear = function ($movies){
+    $filteredMovies = [];
+    foreach($movies as $movie){
+        if ($movie['releaseYear'] > 2015){
+            $filteredMovies[] = $movie;
+        }
+    }
+    return $filteredMovies;
+}
+
+?>
+
+
+<ul>
+    <?php foreach($filterByYear($movies) as $movie) : ?>
+    <li>
+        <?= $movie['name']; ?>
+    </li>
+    <?php endforeach;?>
 </ul>
 </body>
 </html>
