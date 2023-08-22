861648ac96843f026eb247698108bb8125951aa7
Alice Wanjiku <alicenjoroge707@thejitu.com>
2023-08-22 13:32:26 +0300

    php partials

--- index.php
+++ index.php
@@ -1,30 +1,7 @@
 <?php
 
-$books = [
-    [
-        "name" => "Now I am Known",
-        "author" => "Peter",
-        "publishURL" => "site@example.com",
-        "publishYear" => "2003"
-    ],
-    [
-        "name" => "The culture map",
-        "author" => "Emily",
-        "publishURL" => "site@example.com",
-        "publishYear" => "2007"
-    ],
-    [
-        "name" => "Seen and Mehr",
-        "author" => "German",
-        "publishURL" => "site@example.com",
-        "publishYear" => "2005"
-    ]
-];
-//making the inbuilt PHP filter function
+require "functions.php";
 
-$filteredItems = array_filter($books, function ($book){
-    return $book["publishYear"] > 2004 && $book["publishYear"] < 2010;
-});
+$heading = "Welcome";
 
-
-require "index.view.php";
+require "views/index.view.php";
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,15 +1,5 @@
 
-<!doctype html>
-<html lang="en" class="h-full bg-gray-100" >
-<head>
-    <meta charset="UTF-8">
-    <title>Demo</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
-<body class="h-full" >
-<div class="min-h-full">
-    <nav class="bg-gray-800">
+<nav class="bg-gray-800">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div class="flex h-16 items-center justify-between">
                 <div class="flex items-center">
@@ -19,11 +9,9 @@
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                            <a href="#" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
-                            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a>
-                            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
-                            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
-                            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Reports</a>
+                            <a href="/" class= "<?= urlIs('/') ? 'bg-gray-900 text-white' : 'text-gray-300'?> rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+                            <a href="/about.php" class=" <?= urlIs('/about.php') ? 'bg-gray-900 text-white' : 'text-gray-300'?> text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About Us</a>
+                            <a href="/contact.php" class=" <?= urlIs('/contact.php') ? 'bg-gray-900 text-white' : 'text-gray-300'?> text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
                         </div>
                     </div>
                 </div>
@@ -48,15 +36,15 @@
                             </div>
 
                             <!--
-                              Dropdown menu, show/hide based on menu state.
+Dropdown menu, show/hide based on menu state.
 
-                              Entering: "transition ease-out duration-100"
+Entering: "transition ease-out duration-100"
                                 From: "transform opacity-0 scale-95"
                                 To: "transform opacity-100 scale-100"
                               Leaving: "transition ease-in duration-75"
                                 From: "transform opacity-100 scale-100"
                                 To: "transform opacity-0 scale-95"
-                            -->
+-->
                             <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                                 <!-- Active: "bg-gray-100", Not Active: "" -->
                                 <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
@@ -88,8 +76,8 @@
         <div class="md:hidden" id="mobile-menu">
             <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="#" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
-                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
+                <a href="/" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Home</a>
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About Us</a>
                 <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
                 <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
                 <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Reports</a>
@@ -119,20 +107,3 @@
             </div>
         </div>
     </nav>
-
-    <header class="bg-white shadow">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-        </div>
-    </header>
-    <main>
-        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <!-- Your content -->
-        </div>
-    </main>
-</div>
-
-
-</body>
-</html>
-
