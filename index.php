4b247caaf628fe4ac843bce979badf7c5502833d
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-08-23 12:04:14 +0300

    adding Routes

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
 require "functions.php";
-
-
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-if ($uri === '/'){
-    require "controllers/index.php";
-}else if ( $uri === '/about'){
-    require "controllers/about.php";
-}else if ($uri === '/contact'){
-    require "controllers/contact.php";
-}
+require "routes.php";
