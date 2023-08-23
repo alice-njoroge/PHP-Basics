24378dd764e3e50d2097a20b8c290720e45a9cb4
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-08-23 16:48:03 +0300

    quick intro to classes

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 require "functions.php";
-require "routes.php";
+
+class Person {
+    public $name;
+    public $age;
+
+    function breath(){
+      echo  $this->name . " is breathing";
+    }
+}
+
+$person = new Person();
+$person->name = "John Doe";
+
+$person->breath();
