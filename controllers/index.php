5e7c34b5318e1b8f323efd72f827f27a2d320b10
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-08-23 10:33:26 +0300

    adding contollers

---
+++ controllers/index.php
@@ -0,0 +1,4 @@
+<?php
+$heading = "Welcome";
+
+require "views/index.view.php";
--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
-
 require "functions.php";
 
-$heading = "Welcome";
 
-require "views/index.view.php";
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+if ($uri === '/'){
+    require "controllers/index.php";
+}else if ( $uri === '/about'){
+    require "controllers/about.php";
+}else if ($uri === '/contact'){
+    require "controllers/contact.php";
+}
