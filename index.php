ff63cbd8ae829e16f6ff39d438fd62c941f6ae8a
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-08-24 12:09:21 +0300

    environment variables

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 require "functions.php";
 require "Database.php";
+$config = require "config.php";
 
 // connect to db and execute a query
 
-
-$db = new Database();
+$db = new Database($config['database']);
 $posts = $db->query("select * from posts")->fetchall(PDO::FETCH_ASSOC);
 
 foreach ($posts as $post){
