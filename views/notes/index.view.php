ae280994b9e340fae402a1955170f03ebae0a0eb
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-08-31 20:41:53 +0300

    defining file path using current directory

--- views/notes/index.view.php
+++ views/notes/index.view.php
@@ -1,5 +1,5 @@
 <?php
-require('views/partials/head.php') ?>
+require( __DIR__. '/../partials/head.php') ?>
 
 <?php require('views/partials/nav.php') ?>
 <?php require('views/partials/banner.php') ?>
