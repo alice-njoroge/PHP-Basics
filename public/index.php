0280fd18f1122457eaf00d8a6fdabb443713ef56
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-09-03 18:02:55 +0300

    build a better router

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+
+use core\Router;
+
 const BASE_URL = __DIR__ . '/../';
 require BASE_URL . "core/functions.php";
 
@@ -8,7 +11,14 @@ spl_autoload_register(function ($class) {
 
     require base_url("{$class}.php");
 });
-require base_url("core/router.php");
+
+$router = new Router();
+$routes = require base_url("routes.php");
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);
 
 
 
